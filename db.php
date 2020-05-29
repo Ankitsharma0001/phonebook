bbabcf8c4b04dc82e4b8f5ecf8a8fa6c1c27424f
Ankit Sharma <49328285+Ankitsharma0001@users.noreply.github.com>
2020-05-29 15:16:02 +0530

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connection = mysqli_connect('localhost:3308','root','','phonebook1');
-//
-
-if(! $connection ) {
-               die('Could not connect: ' . mysql_error());
-            }
-?>
